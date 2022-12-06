8aa3ef39667e4170c7c48af158ed877dfc877ae5
Yevhenii Kovalchuk <9695470+Senpos@users.noreply.github.com>
2022-12-06 16:02:29 +0200

    day 5

---
+++ day5/part2.py
@@ -0,0 +1,32 @@
+import re
+from collections import deque
+
+with open("input.txt") as f:
+    data = f.read()
+
+drawing, procedure = data.split("\n\n", maxsplit=1)
+
+lines = drawing.splitlines()
+# we don't really need stack identifiers (1, 2, 3, ...)
+lines.pop()
+
+rows = [re.findall(r".(.). ?", line) for line in lines]
+# rotate
+stacks = [
+    [cell for cell in row if cell != " "]
+    for row in zip(*rows[::-1])
+]
+
+moves = procedure.splitlines()
+for move in moves:
+    how_many, from_, to = map(int, re.findall(r"\d+", move))
+    from_ -= 1
+    to -= 1
+
+    moving = deque()
+    for _ in range(how_many):
+        moving.appendleft(stacks[from_].pop())
+    stacks[to].extend(moving)
+
+msg = "".join([stack[-1] for stack in stacks])
+print(msg)
