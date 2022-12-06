2e2caff7de31e20405c50abc93242825bf0e24e8
Yevhenii Kovalchuk <9695470+Senpos@users.noreply.github.com>
2022-12-06 17:18:08 +0200

    day 6

---
+++ day6/part2.py
@@ -0,0 +1,18 @@
+with open("input.txt") as f:
+    for datastream in f:
+        datastream = datastream.strip()
+
+        seen = []
+        for position, packet in enumerate(datastream):
+            try:
+                seen_idx = seen.index(packet)
+            except ValueError:
+                ...
+            else:
+                seen = seen[seen_idx + 1 :]
+
+            seen.append(packet)
+
+            if len(seen) == 14:
+                print("💚", position + 1)
+                break
